127d36649cb747f04bdc1b6f5f66dfe13e861a8d
Jill Moore <Jill.Moore@umassmed.edu>
2020-07-01 11:32:14 -0400

    added step 10

---
+++ Version-2/cCRE-Pipeline/Scripts/10_Homologous-cCREs.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+#SPDX-License-Identifier: MIT
+#Copyright (c) 2016-2020 Jill Moore, Michael Purcaro, Henry Pratt, Zhiping Weng
+
+#Step 10 of ENCODE cCRE pipeline (V2)
+#Jill E. Moore
+#Weng Lab
+
+#TO RUN:
+#./10_Homologous-ccREs.sh
+
+genome1=hg38
+genome2=mm10
+
+version1=V5
+version2=V5
+
+chain1=~/Lab/Reference/Human/hg38/hg38ToMm10.over.chain
+chain2=~/Lab/Reference/Mouse/mm10ToHg38.over.chain
+
+output=~/Lab/ENCODE/Encyclopedia/$version1/Registry/$version1"-"$genome1/LiftOver
+mkdir -p $output
+
+bed1=~/Lab/ENCODE/Encyclopedia/$version1/Registry/$version1"-"$genome1/$genome1-ccREs-Simple.bed
+num1=$(wc -l $bed1 | awk 'function ceil(x, y){y=int(x); return(x>y?y+1:y)} \
+     {print ceil($1/1000)}')
+#1-$num1
+jobid=$(sbatch --nodes 1 --array=99 --mem=5G --time=00:30:00 \
+    --output=/home/moorej3/Job-Logs/jobid_%A_%a.output \
+    --error=/home/moorej3/Job-Logs/jobid_%A_%a.error \
+    LiftOver.sh $genome1 $version1 $chain1 0.5 $genome2 $output | awk '{print $4}')
+
+echo $jobid
+
+sleep 20
+list=100
+while [ $list -gt 1 ]
+do
+list=$(squeue -j $jobid | wc -l | awk '{print $1}')
+echo -e "jobs still running: $list"
+sleep 10
+done
+
+bed2=~/Lab/ENCODE/Encyclopedia/$version2/Registry/$version2"-"$genome2/$genome2-ccREs-Simple.bed
+num2=$(wc -l $bed1 | awk 'function ceil(x, y){y=int(x); return(x>y?y+1:y)} \
+     {print ceil($1/1000)}')
+
+jobid=$(sbatch --nodes 1 --array=1-$num2 --mem=5G --time=00:30:00 \
+    --output=/home/moorej3/Job-Logs/jobid_%A_%a.output \
+    --error=/home/moorej3/Job-Logs/jobid_%A_%a.error \
+    LiftOver.sh $genome2 $version2 $chain2 0.5 $genome1 $output | awk '{print $4}')
+
+echo $jobid
+
+sleep 20
+list=100
+while [ $list -gt 1 ]
+do
+list=$(squeue -j $jobid | wc -l | awk '{print $1}')
+echo -e "jobs still running: $list"
+sleep 10
+done
+
+cd ~/Lab/ENCODE/Encyclopedia/V5/Registry/V5-hg38/LiftOver
+
+rm *NO.txt
+
+cat $genome1"-"$genome2*.txt | sort -u > $genome1"-"$genome2.bed
+cat $genome2"-"$genome1*.txt | sort -u > $genome2"-"$genome1.bed
+
+bedtools intersect -wo -a $genome1"-"$genome2.bed -b $bed2 > A
+bedtools intersect -wo -b $bed1 -a $genome2"-"$genome1.bed > B
+
+awk 'FNR==NR {x[$5$11];next} ($11$5 in x)' B A | \
+    awk '{print $5 "\t" $11}' > $genome1-$genome2-Homologous.txt
+
+mv $genome1-$genome2-Homologous.txt ~/Lab/ENCODE/Encyclopedia/$version1/Registry/$version1"-"$genome1/
+
---
+++ Version-2/cCRE-Pipeline/Scripts/LiftOver.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SPDX-License-Identifier: MIT
+#Copyright (c) 2016-2020 Jill Moore, Michael Purcaro, Henry Pratt, Zhiping Weng
+
+mkdir -p /tmp/moorej3/$SLURM_JOBID"-"$SLURM_ARRAY_TASK_ID
+cd /tmp/moorej3/$SLURM_JOBID"-"$SLURM_ARRAY_TASK_ID
+
+j=$SLURM_ARRAY_TASK_ID
+
+genome=$1
+version=$2
+chain=$3
+match=$4
+newGenome=$5
+outputDir=$6
+
+output=$genome"-"$newGenome
+
+bed=~/Lab/ENCODE/Encyclopedia/$version/Registry/$version"-"$genome/$genome-ccREs-Simple.bed
+
+N=$(awk 'BEGIN{print '$j'*1000}')
+head -n $N $bed | tail -n 1000 > bed
+
+~/bin/liftOver bed $chain $output.$j.txt $output.$j.NO.txt -minMatch=$match
+mv $output.$j.txt $output.$j.NO.txt $outputDir
+
+rm -r /tmp/moorej3/$SLURM_JOBID"-"$SLURM_ARRAY_TASK_ID
