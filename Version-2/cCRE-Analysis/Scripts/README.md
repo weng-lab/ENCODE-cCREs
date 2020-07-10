264823b02701db65c2089eccda8b03464d14f5d1
Jill-Moore <Jill.Moore@umassmed.edu>
2020-07-10 10:07:46 -0400

    added SF8b

--- Version-2/cCRE-Analysis/Scripts/README.md
+++ Version-2/cCRE-Analysis/Scripts/README.md
@@ -4,7 +4,7 @@
 
 ## Supplementary Figure 8 | Overlap of cCREs with ChromHMM states
 
-Overlap of cCREs with ChromHMM states. a, Percentages of various groups of GM12878 cCREs that overlap ChromHMM states. b, Percentage of GM12878 cCREs-pELS that overlap ChromHMM states ranked by distance from the nearest TSS. Due to ChromHMM's lower spatial resolution, cCREs-PLS that are closest to TSSs overlap promoter ChromHMM states while those farther away overlap enhancer states. c, Percentages of mouse cCREs that overlap ChromHMM states in the corresponding tissue. All combinations of tissues and timepoints with both DNase and histone modification data were included and the overlap was computed for data in the same tissue at the same time point.
+**Overlap of cCREs with ChromHMM states. a,** Percentages of various groups of GM12878 cCREs that overlap ChromHMM states. **b,** Percentage of GM12878 cCREs-pELS that overlap ChromHMM states ranked by distance from the nearest TSS. Due to ChromHMM's lower spatial resolution, cCREs-PLS that are closest to TSSs overlap promoter ChromHMM states while those farther away overlap enhancer states. **c,** Percentages of mouse cCREs that overlap ChromHMM states in the corresponding tissue. All combinations of tissues and timepoints with both DNase and histone modification data were included and the overlap was computed for data in the same tissue at the same time point.
 
 ### Panel A
 
@@ -16,4 +16,18 @@ Requires:
 * `choose-majority-chromhmm-state.py`
 * GM12878 cCREs
 * GM12878 ChromHMM states: [ENCFF001TDH](https://www.encodeproject.org/files/ENCFF001TDH/)
+
+
+### Panel B
+
+```
+./Supplementary-Figure-8b.pELS-ChromHMM-TSS-Distance.sh
+```
+
+Requires:
+* `choose-majority-chromhmm-state.py`
+* GM12878 cCREs
+* GM12878 ChromHMM states: [ENCFF001TDH](https://www.encodeproject.org/files/ENCFF001TDH/)
+* [GENCODE24 TSSs](https://github.com/weng-lab/ENCODE-cCREs/blob/master/Version-2/cCRE-Pipeline/Input-Data/hg38/GENCODE24/TSS.Basic.bed.gz)
+
 
--- Version-2/cCRE-Analysis/Scripts/Supplementary-Figure-8b.pELS-ChromHMM-TSS-Distance.sh
+++ Version-2/cCRE-Analysis/Scripts/Supplementary-Figure-8b.pELS-ChromHMM-TSS-Distance.sh
@@ -6,31 +6,37 @@
 #Script calculates overlap between cell type-specific cCREs & ChromHMM states
 
 #TO RUN:
-#./Supplementary-Figure-8a.Human-ChromHMM-Overlap.sh
+#./Supplementary-Figure-8b.pELS-ChromHMM-TSS-Distance.sh
 
 biosample=GM12878
 dataDir=~/Lab/ENCODE/Encyclopedia/V5/Registry/V5-hg38/
 scriptDir=~/GitHub/ENCODE-cCREs/Version-2/cCRE-Analysis/Scripts
 masterList=$dataDir/Cell-Type-Specific/Master-Cell-List.txt
 chain=~/Lab/Reference/Human/hg38/hg38ToHg19.over.chain
+tss=~/Lab/Reference/Human/hg38/GENCODE24/TSS.Basic.bed
 chromhmm=$dataDir/Manuscript-Analysis/ChromHMM/ENCFF001TDH.bed
 
 file=$(grep $biosample $masterList | awk '{print $2"_"$4"_"$6"_"$8".7group.bed"}')
 ccres=$dataDir/Cell-Type-Specific/Seven-Group/$file
 
-~/bin/liftOver -bedPlus=9 $ccres $chain tmp.yes-map tmp.no-map
-awk '{print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $10}' tmp.yes-map > tmp.ccres-hg19
+grep pELS $ccres | awk '{print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" $6}' > tmp.ccres
+sort -k4,4 tmp.ccres > tmp.sorted
+bedtools closest -d -a tmp.ccres -b $tss | awk '{print $4 "\t" $NF}' | \
+    sort -k1,1 > tmp.distance
+paste tmp.sorted tmp.distance > tmp.union
 
-echo -e "Group \t Total-cCREs \t TSS \t Bivalent-TSS \t Transcription \t" \
+echo -e "Distance \t Total-cCREs \t TSS \t Bivalent-TSS \t Transcription \t" \
     "Strong-Enhancer \t Weak-Enhancer \t Insulator \t Repressed" >> tmp.results
-datasets=("PLS" "pELS" "dELS" "DNase-H3K4me3" "CTCF-only" "DNase-only" "Low-DNase")
-for l in ${datasets[@]}
+
+for j in `seq 0 100 2000`
 do
-    echo "Processing" $l "..."
-    grep $l tmp.ccres-hg19 > tmp.bed
+    echo "Processing" $j "..."
+    awk '{if ($NF > '$j' && $NF <= '$j'+100) print $1 "\t" $2 "\t" $3 "\t" $4}' \
+        tmp.union | sort -k1,1 -k2,2n > tmp.group
+    ~/bin/liftOver tmp.group $chain tmp.bed tmp.no-map
     k=$(wc -l tmp.bed | awk '{print $1}')
     bedtools intersect -wo -a tmp.bed -b $chromhmm > tmp.intersect
-    python $scriptDir/choose-majority-chromhmm-state.py tmp.intersect 4 9 15 > tmp.filter
+    python $scriptDir/choose-majority-chromhmm-state.py tmp.intersect 4 8 14 > tmp.filter
     A=$(awk '{if ($2 == "1_Active_Promoter" || $2 == "2_Weak_Promoter") print $0}' tmp.filter \
         | wc -l | awk '{print $1}')
     B=$(awk '{if ($2 == "3_Poised_Promoter") print $0}' tmp.filter | wc -l | awk '{print $1}')
@@ -44,9 +50,9 @@ do
     G=$(awk '{if ($2 == "15_Repetitive/CNV" || $2 == "14_Repetitive/CNV" || \
         $2 == "13_Heterochrom/lo" || $2 == "12_Repressed") print $0}' tmp.filter \
         | wc -l | awk '{print $1}')
-    echo -e $l "\t" $k "\t" $A "\t" $B "\t" $C "\t" $D "\t" $E "\t" $F "\t" $G >> tmp.results
+    echo -e $j "\t" $k "\t" $A "\t" $B "\t" $C "\t" $D "\t" $E "\t" $F "\t" $G >> tmp.results
 done
 
-mv tmp.results $biosample.cCRE-ChromHMM-Overlap-Summary.txt
+mv tmp.results $biosample.cCRE-ChromHMM-pELS-Distance-Summary.txt
 
 rm tmp*
