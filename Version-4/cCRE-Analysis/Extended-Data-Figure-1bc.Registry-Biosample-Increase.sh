588fc6505e970fe50b48af7c5c22102a7a9ee5b5
Jill Moore <Jill.Moore@umassmed.edu>
2025-09-26 06:45:37 -0400

    checked EDF1bc

--- Version-4/cCRE-Analysis/Extended-Data-Figure-1bc.Registry-Biosample-Increase.sh
+++ Version-4/cCRE-Analysis/Extended-Data-Figure-1bc.Registry-Biosample-Increase.sh
@@ -13,17 +13,17 @@ cd $workingDir
 echo -e "Type" "\t" "Count" "\t" "Version" > tmp.summary
 echo -e "Type" "\t" "Count" "\t" "Version" > tmp.core
 
-versions=(V5 V6 V7)
+versions=(V5 V7)
 for version in ${versions[@]}
 do
     echo $version
     dir=~/Lab/ENCODE/Encyclopedia/$version/Registry/$version-hg38
     cat $dir/DNase-List.txt $dir/H3K4me3-List.txt $dir/H3K27ac-List.txt $dir/CTCF-List.txt > $version.All.txt
-    python ~/Projects/ENCODE/Encyclopedia/Version6/Curate-Data/classify-biosample-type.py $version.All.txt > tmp.tmp
+    python $scriptDir/Toolkit/assign-organ-tissue.py $version.All.txt GRCh38 > tmp.tmp
     awk -F "\t" '{print $3 "\t" $(NF-2)}' tmp.tmp | sort -u > tmp.unique
-    python ~/bin/count.py tmp.unique -1 | awk -F "\t" '{print $0 "\t" "'$version'"}' >> tmp.summary
-    awk 'FNR==NR {x[$9];next} ($1 in x)' $dir/Cell-Type-Specific/Group-1.txt tmp.unique > tmp.group1
-    python ~/bin/count.py tmp.group1 -1 | awk -F "\t" '{print $0 "\t" "'$version'"}' >> tmp.core
+    python $scriptDir/Toolkit/count.py tmp.unique -1 | awk -F "\t" '{print $0 "\t" "'$version'"}' >> tmp.summary
+    awk 'FNR==NR {x[$9];next} ($1 in x)' $dir/Cell-Type-Specific/Core-Collection.txt tmp.unique > tmp.group1
+    python $scriptDir/Toolkit/count.py tmp.group1 -1 | awk -F "\t" '{print $0 "\t" "'$version'"}' >> tmp.core
 done
 
 mv tmp.summary ../Figure-Input-Data/Extended-Data-Figure-1b.Registry-Biosample-Increase.txt 
